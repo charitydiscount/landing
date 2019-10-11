f1c495a68495ae6bb58ada5975598ba6640355f3
Andrei Stefanie <andrei.stefanie@gmail.com>
2019-10-11 15:41:22 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# landing
-Landing page showcasing the main features of CharityDiscount
+# Charity Discount Landing Page
+
+Landing page showcasing the main features of CharityDiscount.
+
+## Usage
+
+- `gulp` or `gulp build` builds the page(s) (minify, inline, translate)
+- `gulp watch` for local development (serves the ro-RO version, can be changed in _gulpfile.js_)
+
+## Features
+
+- As fast as it gets - **all** the css is purged, minified and inlined directly in the html file. (custom js is also minified/inlined - not purged because it doesn't block the rendering)
+- Translation - support for multiple languages (resulting in different html files for each individual language)
+- SCSS/SASS support
+
+### Thanks
+
+This project is based on [Start Bootstrap's New Age Landing Page](https://startbootstrap.com/themes/new-age/). Check it out.
---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+'use strict';
+
+const autoprefixer = require('gulp-autoprefixer');
+const browsersync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const del = require('del');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const i18n = require('gulp-html-i18n');
+const inlinesource = require('gulp-inline-source');
+const purgecss = require('gulp-purgecss');
+
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: './',
+      index: 'index-ro.html',
+    },
+    notify: false,
+    port: 3000,
+  });
+  done();
+}
+
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+function css() {
+  return gulp
+    .src('./scss/**/*.scss')
+    .pipe(plumber())
+    .pipe(
+      sass({
+        outputStyle: 'expanded',
+        includePaths: './node_modules',
+      })
+    )
+    .on('error', sass.logError)
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest('./css'))
+    .pipe(
+      rename({
+        suffix: '.min',
+      })
+    )
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./css'))
+    .pipe(browsersync.stream());
+}
+
+function js() {
+  return gulp
+    .src(['./js/*.js', '!./js/*.min.js'])
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: '.min',
+      })
+    )
+    .pipe(gulp.dest('./js'))
+    .pipe(browsersync.stream());
+}
+
+function watchFiles() {
+  gulp.watch('./scss/**/*', css);
+  gulp.watch(['./js/**/*', '!./js/**/*.min.js'], js);
+  gulp.watch('./index.html', translate);
+  gulp.watch('./**/*.html', browserSyncReload);
+}
+
+function translate() {
+  return gulp
+    .src('./index.html')
+    .pipe(
+      i18n({
+        langDir: './lang',
+      })
+    )
+    .pipe(inlinesource())
+    .pipe(gulp.dest('.'));
+}
+
+function clean() {
+  return del(['./vendor/']);
+}
+
+function modules() {
+  return gulp
+    .src('./node_modules/bootstrap-css-only/css/bootstrap.min.css')
+    // .pipe(
+    //   purgecss({
+    //     content: ['./index.html'],
+    //   })
+    // )
+    .pipe(gulp.dest('./vendor/bootstrap'));
+}
+
+const vendor = gulp.series(clean, modules);
+const build = gulp.series(vendor, gulp.parallel(css, js), translate);
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
+
+exports.css = css;
+exports.js = js;
+exports.clean = clean;
+exports.vendor = vendor;
+exports.build = build;
+exports.watch = watch;
+exports.translate = translate;
+
+exports.default = build;
